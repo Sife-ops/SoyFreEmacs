b937568002d8bc8a5aa0279be5ca5fc7f7b8ef5c
Wyatt Goettsch <wyatt.goettsch@gmail.com>
2020-11-13 23:17:45 -0800

    added evil-collection, enable evil by default

--- modules/use-package-evil.el
+++ modules/use-package-evil.el
@@ -7,18 +7,26 @@
 
 (use-package evil
     :ensure t
+
     :init
     (setq evil-want-C-u-scroll t)
     (setq evil-want-integration t)
     (setq evil-want-keybinding nil)
     (setq evil-search-module 'evil-search)
+
     :config
+    (evil-mode 1)
 
     ;; hollow cursor style in emacs mode
-    (with-eval-after-load 'sife-functions
-      (setq sife-cursor-style 'hollow)
-      (setq-default cursor-type sife-cursor-style))
-    
+    ;; (with-eval-after-load 'sife-functions
+      ;; (setq sife-cursor-style 'hollow)
+      ;; (setq-default cursor-type sife-cursor-style))
+
+    (use-package evil-collection
+      :ensure t
+      :config
+      (evil-collection-init))
+
     (use-package evil-surround
       :ensure t
       :config
