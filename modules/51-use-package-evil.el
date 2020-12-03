071ab10e3f45e070c7dc0f8b1639fa72b1391bac
Wyatt Goettsch <wyatt.goettsch@gmail.com>
2020-12-03 00:18:25 -0800

    added cursor changer for terminal emacs

--- modules/51-use-package-evil.el
+++ modules/51-use-package-evil.el
@@ -43,7 +43,20 @@
     (use-package evil-commentary
       :ensure t
       :config
-      (evil-commentary-mode)))
+      (evil-commentary-mode))
+
+    (use-package evil-terminal-cursor-changer
+      :ensure t
+      :config
+      (unless (display-graphic-p)
+        (require 'evil-terminal-cursor-changer)
+        (evil-terminal-cursor-changer-activate)) ; or (etcc-on)
+
+      (setq evil-motion-state-cursor 'box)  ; █
+      (setq evil-visual-state-cursor 'box)  ; █
+      (setq evil-normal-state-cursor 'box)  ; █
+      (setq evil-insert-state-cursor 'bar)  ; ⎸
+      (setq evil-emacs-state-cursor  'hbar))) ; _
 
 (provide 'init-evil)
 
