9f3b0982f16162fa9246da77487911e2fb9d5ff6
Wyatt Goettsch <55699197+Sife-ops@users.noreply.github.com>
2021-09-28 12:59:58 -0700

    Update init.el

--- init.el
+++ init.el
@@ -15,19 +15,6 @@
 ;; $ cd ~/.config/emacs/init.d
 ;; $ ln -sf ../modules/use-package-company.el ./81-use-package-company.el
 ;;
-;; Modules are loaded in alpha-numerical order, so it is recommended to make use
-;; of a numbering scheme as in the above example to manage dependencies. While
-;; efforts are made to provide modules that are as sequence-independent as
-;; possible, conditions may arise in which one module is dependent on another
-;; and should be given a number higher than the prerequisite module. Namely, it
-;; is important to ensure that modules with 'package' in the name, which depend
-;; on package archives, are loaded after 'use-package-archives.el,' which
-;; installs and initializes the package archives.
-;;
-;; 80-use-package-archives.el
-;; 81-use-package-company.el
-;; 81-package-ddskk.el
-
 ;;; Code:
 
 (setq user-emacs-directory "~/.config/emacs/")
